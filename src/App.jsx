d5493ebc1e16e7af22516476a0281f2dcd2e554d
John Kim <johnkim@wireless-10-104-33-158.umd.edu>
2024-04-01 19:06:43 -0400

    working with react-three-fiber

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useRef } from "react"
 import './App.css'
-import { Canvas } from "@react-three/fiber"
+
+const Cube = ({position, size, color}) => {
+  
+  const ref = useRef()
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta
+    console.log(state)
+  })
+
+  return <mesh position={position} ref = {ref}> 
+    <boxGeometry args={size}/>
+    <meshStandardMaterial color={color}/>
+  </mesh>
+}
 
 const App = () => {
-  return (
-    <>
-      <Canvas>
-        <boxGeometry/>
-      </Canvas>
-    </>
-  )
+  return <Canvas>
+    <directionalLight position={[0, 0, 2]}/>
+    <ambientLight/>
+    
+    <group position={[0, -1, 0]}>
+      <Cube position={[1,0,0]} size={[2,1,1]} color={"red"}/>
+      <Cube position={[0,3,0]} size={[2,1,1]} color={"red"}/>
+    </group>
+  </Canvas>
 }
 
 export default App
